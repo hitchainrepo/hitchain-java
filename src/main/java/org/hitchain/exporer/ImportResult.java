5a5f93a5cceb8aa7682aa7badbb11c6d5de71768
tylerchen <iffiff1@gmail.com>
2020-07-16 09:15:40 +0800

    feat: add ImportResult

---
+++ src/main/java/org/hitchain/exporer/ImportResult.java
@@ -0,0 +1,15 @@
+
+package org.hitchain.exporer;
+
+public enum ImportResult {
+    IMPORTED_BEST,
+    IMPORTED_NOT_BEST,
+    EXIST,
+    NO_PARENT,
+    INVALID_BLOCK,
+    CONSENSUS_BREAK;
+
+    public boolean isSuccessful() {
+        return equals(IMPORTED_BEST) || equals(IMPORTED_NOT_BEST);
+    }
+}
