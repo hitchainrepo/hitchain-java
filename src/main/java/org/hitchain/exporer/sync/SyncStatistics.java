0dae2089bb09714f00eba60c0879f834619ced8c
Origin-Strat <iberich666@163.com>
2020-07-22 17:49:11 +0800

    add sync statistics

---
+++ src/main/java/org/hitchain/exporer/sync/SyncStatistics.java
@@ -0,0 +1,50 @@
+package org.hitchain.exporer.sync;
+
+public class SyncStatistics {
+    private long updatedAt;
+    private long blocksCount;
+    private long headersCount;
+    private int headerBunchesCount;
+
+    public SyncStatistics() {
+        reset();
+    }
+
+    public void reset() {
+        updatedAt = System.currentTimeMillis();
+        blocksCount = 0;
+        headersCount = 0;
+        headerBunchesCount = 0;
+    }
+
+    public void addBlocks(long cnt) {
+        blocksCount += cnt;
+        fixCommon(cnt);
+    }
+
+    public void addHeaders(long cnt) {
+        headerBunchesCount++;
+        headersCount += cnt;
+        fixCommon(cnt);
+    }
+
+    private void fixCommon(long cnt) {
+        updatedAt = System.currentTimeMillis();
+    }
+
+    public long getBlocksCount() {
+        return blocksCount;
+    }
+
+    public long getHeadersCount() {
+        return headersCount;
+    }
+
+    public long secondsSinceLastUpdate() {
+        return (System.currentTimeMillis() - updatedAt) / 1000;
+    }
+
+    public int getHeaderBunchesCount() {
+        return headerBunchesCount;
+    }
+}
